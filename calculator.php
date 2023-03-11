6c541b6cb3d2c9adcab4e469a14aa76e17e10b82
ESLAM <102685868+ESLAM80@users.noreply.github.com>
2023-03-11 17:02:08 +0200

    apdate

--- calculator.php
+++ calculator.php
@@ -14,12 +14,15 @@ if(isset($_POST['submit'])){
         case "sub":
             $num3=$num1-$num2;
             break;  
-
         case "mul":
             $num3=$num1*$num2;
             break;
         case "div":
             $num3=$num1/$num2;
+        case "mod":
+            $num3=$num1%$num2;
+        case "exp":
+            $num3=$num1**$num2;
             break;
         default:
             $num3="Please select operation ";
@@ -40,13 +43,13 @@ if(isset($_POST['submit'])){
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300&display=swap" rel="stylesheet">
     
     <style> 
-        *{
+
+*{
     margin: 0%;
     padding: 0%;
     font-family: 'Montserrat', sans-serif;
     
 }
-
 
 .cal{
     width:600px;
@@ -59,9 +62,14 @@ if(isset($_POST['submit'])){
     transform: translate(-50%,-50%);
     text-align: center;
     box-shadow: 20px 6px 0px 0px rgb(128 128 128 / 26%);
-
-
 }
+.header{
+    text-align:center;
+    margin-top:50px;
+    color:white;
+}
+
+
 form{
     position: absolute;
     left: 50%;
@@ -119,29 +127,46 @@ select{
     color: black;
     border: none;
 }
+
+.result{
+    margin-top:30px;
+    font-size:18px;
+}
+.value-result{
+    color:white;
+    font: size 23px;
+}
         
     </style>
     
 </head>
 <body>
 
-
 <div class="cal">
-    <h3 style="text-align:center;margin-top:50px;color:white">CALCULATOR</h3>
+    <h3 class="header">CALCULATOR</h3>
     <form action="calculator.php" method="POST">
 
         <input type="text" name="num1" placeholder="Enter first Number" value="<?php echo @$num1?>" required>
         <input type="text" name="num2" placeholder="Enter Second Number"  value="<?php echo @$num2?>" required>
+
         <select name="drop">
             <option>Please select</option>
-            <option value="add">ADD</option>
-            <option value="sub">SUB</option>
-            <option value="mul">MUL</option>
-            <option value="div">DIV</option>
+            <option value="add">ADD '+'</option>
+            <option value="sub">SUB '-'</option>
+            <option value="mul">MUL '*'</option>
+            <option value="div">DIV '/'</option>
+            <option value="mod">mod '%'</option>
+            <option value="exp">exp '**'</option>
         </select> <br>
+
         <input type="submit" name="submit">
 
-        <h5 style="margin-top:30px;font-size:18px;">YOUR ANSWER: <br> <br><span style="color:white;font: size 23px;"><?php echo @$num3?></span></h5>
+        <h5 class="result" >
+            YOUR ANSWER:  <br> <br>
+            <span class="value-result">
+                <?php echo @$num3?>
+            </span>
+        </h5>
     </form>
     
 </div>
